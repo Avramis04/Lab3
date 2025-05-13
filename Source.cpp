74343bd9a63bffb4794969d7569be57ba810ceea
Avramis04 <Avramiz.kontou@icloud.com>
2025-05-13 14:20:12 +0300

    Complete the itispositive function

--- Source.cpp
+++ Source.cpp
@@ -164,7 +164,12 @@ int sumOddArray(const int arr[], const int size){
 // If all the values in the array are positive return true
 bool isAllPositive(const int arr[], const int size){
 	//@TODO: You will need to complete this. Including makeing the appropriate comment header
-  return 0;
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] <= 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 // Finds the average of all the odd numbers in the array and stores this in the last argument
